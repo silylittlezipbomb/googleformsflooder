470bc2a4061329b6ca3204ff70d0ad7dff594c84
silylittlezipbomb <93442407+silylittlezipbomb@users.noreply.github.com>
2022-12-05 21:20:01 -0500

    added random data
    I added the option to add a random string to the end of listed values.

--- flooder.py
+++ flooder.py
@@ -3,22 +3,30 @@ import threading
 import colorama
 from colorama import *
 import os
+import random
+import string
 
 colorama.init(strip=False)
 
 counter = 0 # don't change this, this just counts up
 
-threadsamt = int(input("threads (2 shouldn't rate limit but is slow): "))
+threadsamt = int(input("How many threads? (2 shouldn't rate limit but is slow): "))
 
-url = input("enter the url here (should have /formResponse at the end): ")
+url = input("Enter the url here (should have /formResponse at the end): ")
 
 data = {}
+valuesWithRand = []
 
 if len(data) == 0:
 
     def get_data():
         key = input("key: ")
         value = input("value: ")
+        useRand = input("add random characters to this value? (y/n): ")
+
+        if useRand.lower() == "y":
+            valuesWithRand.append(key)
+        
         data[key] = value
 
     get_data()
@@ -36,7 +44,15 @@ os.system('cls')
 
 def submit():
     while True:
+        for x in valuesWithRand:
+            randstring = (''.join(random.choices(string.ascii_letters, k=5)))
+            data[x] = f"{data[x]} [{randstring}]"
+
         r = requests.post(url, data=data)
+
+        for x in valuesWithRand:
+            data[x] = data[x][:-8]
+
         if r.status_code == 200:
             global counter
             counter += 1
